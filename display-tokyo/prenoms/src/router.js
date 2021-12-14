67d3216d147b3b6c69e33324f1bc1095ceabbf57
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-12-14 14:08:47 +0900

    file changed

---
+++ display-tokyo/prenoms/src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Routing from './views/Routing.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  // デフォルトの挙動では URL に `#` が含まれる.
+  // URL から hash を取り除くには `mode:history` を指定する
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/routing',
+      name: 'routing',
+      component: Routing 
+    }
+  ]
+})
+
---
+++ display-tokyo/prenoms/src/views/Routing.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="$style.parent">
+    <div :class="$style.child">
+      <h1>Routing Test Page</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RoutingController'
+}
+</script>
+<style module>
+.parent {
+  margin-top: 50px;
+  height: 500px;
+  width: 100%;
+  /* 子要素を中央に配置する */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.child {
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+  /* 子要素を中央に配置する */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.item {
+  width: 150px;
+}
+
+.value {
+  width: 400px;
+}
+</style>
